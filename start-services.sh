33dff2f9f1b743de8979471e744718323c942d5f
MG500 <63402274+MG500@users.noreply.github.com>
2022-05-07 16:27:19 +0200

    kk

--- start-services.sh
+++ start-services.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo $ID > /var/www/ID.txt
-echo $SECRET > /var/www/SECRET.txt
-echo $DOMAIN > /var/www/DOMAIN.txt
+echo CLIENT_ID=$ID >> var/www/html/.env
+echo CLIENT_SECRET=$SECRET >> var/www/html/.env
+echo REDIRECT_URI=$DOMAIN >> var/www/html/.env
 
 /etc/init.d/apache2 start
 tail -f /var/log/apache2/*
