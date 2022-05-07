0de6f66d7455c0e5b99c01a6e4e445c740a8506b
Max Göb <63402274+M4XGG@users.noreply.github.com>
2022-05-07 16:33:25 +0200

    fix

--- start-services.sh
+++ start-services.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo CLIENT_ID=$ID >> var/www/html/.env
-echo CLIENT_SECRET=$SECRET >> var/www/html/.env
-echo REDIRECT_URI=$DOMAIN >> var/www/html/.env
+echo CLIENT_ID=$ID >> /var/www/html/.env
+echo CLIENT_SECRET=$SECRET >> /var/www/html/.env
+echo REDIRECT_URI=$DOMAIN >> /var/www/html/.env
 
 /etc/init.d/apache2 start
 tail -f /var/log/apache2/*
